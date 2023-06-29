708a8e90fdc47a183cd205dbfa8a75f0c3b8894e
M.J. R <mohamadjavadnoctis2@gmail.com>
2023-06-29 06:09:11 -0700

    add: the main slider of the CoursesSliderWithCategory

--- src/components/CoursesSliderWithCategory/CoursesSliderWithCategory.tsx
+++ src/components/CoursesSliderWithCategory/CoursesSliderWithCategory.tsx
@@ -1,10 +1,28 @@
 // import SectionTitle from "@components/section-title";
-import CourseArea from "@containers/course/layout-01";
+// import CourseArea from "@containers/course/layout-01";
 import { ICourse, MottoType, SectionTitleType, TSection } from "@utils/types";
-// import { motion } from "framer-motion";
-import React, { FC } from "react";
+import { motion } from "framer-motion";
+import CourseCard from "@components/course-card/course-01";
+import React, { FC, useMemo } from "react";
+import { scrollUpVariants } from "@utils/variants";
+import SectionTitle from "@components/section-title";
+import SwiperSlider, { SwiperSlide } from "@components/ui/swiper";
+import dynamic from "next/dynamic";
 
-// const AnimatedSectionTitle = motion(SectionTitle);
+
+const DynamicSwiperSlider = dynamic(() => import('@ui/swiper'), {
+    loading: () => <p>Loading...</p>,
+    ssr: false
+  })
+   
+
+
+
+const AnimatedSwiper = motion(DynamicSwiperSlider);
+
+
+const AnimatedSectionTitle = motion(SectionTitle);
+const AnimatedCourseCard = motion(CourseCard);
 
 type CoursesSliderWithCategoryProps = TSection & {
     data: {
@@ -15,20 +33,93 @@ type CoursesSliderWithCategoryProps = TSection & {
 };
 
 const CoursesSliderWithCategory: FC<CoursesSliderWithCategoryProps> = ({
-    // data: { section_title, motto, courses },
-    data,
+    data: { section_title, courses },
+    // data,
     // space,
     // bg,
     // titleSize,
 }) => {
     // console.log("this is the data: ", data);
+
+    const options = useMemo(() => {
+        return {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            centeredSlides: true,
+            speed: 1000,
+            loop: true,
+            autoplay: false,
+            spaceBetween: 50,
+            pagination: true,
+            breakpoints: {
+                576: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                992: {
+                    slidesPerView: 2,
+                },
+                1499: {
+                    slidesPerView: 3,
+                },
+            },
+        };
+    }, []);
+
+
+
     return (
-        <section className="tw-container tw-mt-[70px]">
-            <div className="tw-min-h-[500px] tw-bg-secondary tw-rounded-[25px]">
-                <CourseArea
+        <section className="tw-container tw-mt-[70px] ">
+            <div className="tw-min-h-[500px] tw-bg-secondary tw-rounded-[25px] tw-py-[50px]">
+                {/* <CourseArea
                     data={data}
                     innerContainerClassNames="tw-bg-secondary"
-                />
+                /> */}
+                {section_title && (
+                    <AnimatedSectionTitle
+                        {...section_title}
+                        titleSize={"small"}
+                        className="tw-mb-7.5 md:tw-mb-15"
+                        initial="offscreen"
+                        whileInView="onscreen"
+                        viewport={{ once: true, amount: 0.4 }}
+                        variants={scrollUpVariants}
+                        color="C"
+                    />
+                )}
+                {/* <div className="tw-grid md:tw-grid-cols-2 lg:tw-grid-cols-3 tw-gap-[103px]"> */}
+                    <AnimatedSwiper
+                        options={options}
+                        className="bookingsAreaSlider tw-h-full tw-static tw-px-[71px]"
+                    >
+                        {courses.map((course) => {
+                            return (
+                                <SwiperSlide
+                                    key={course.id}
+                                    className="books-slides tw-max-w-full tw-h-full !tw-static"
+                                >
+                                    <AnimatedCourseCard
+                                        key={course.path}
+                                        title={course.title}
+                                        path={course.path}
+                                        published_at={course.published_at}
+                                        thumbnail={course.thumbnail}
+                                        price={course.price}
+                                        currency={course.currency}
+                                        initial="offscreen"
+                                        whileInView="onscreen"
+                                        viewport={{ once: true, amount: 0.4 }}
+                                        variants={scrollUpVariants}
+                                        instructorName={course.instructorName}
+                                        duration={course.duration}
+                                    />
+                                </SwiperSlide>
+                            );
+                        })}
+                    </AnimatedSwiper>
+                {/* </div> */}
             </div>
         </section>
     );
--- src/components/course-card/course-01.tsx
+++ src/components/course-card/course-01.tsx
@@ -42,55 +42,58 @@ const CourseCard = forwardRef<HTMLDivElement, TProps>(
         console.log('this is the thumbnail: ', thumbnail)
         // const priceConv = price === 0 ? "free" : `${currency}${price}`;
         return (
-            <div
-                className={clsx(
-                    "tw-transition-all tw-bg-gray-100 tw-rounded-[15px] tw-h-full group hover:tw-bg-white hover:tw-shadow-4xl hover:tw-shadow-black/[0.12]",
-                    className
-                )}
-                ref={ref}
-            >
-                <figure className="tw-relative tw-overflow-hidden tw-rounded-t-[15px]">
-                    {thumbnail?.src && (
-                        <img
-                            src={thumbnail.src}
-                            alt={thumbnail?.alt || title}
-                            width={thumbnail?.width || 300}
-                            height={thumbnail?.height || 300}
-                            loading={thumbnail?.loading || "lazy"}
-                            className="tw-min-h-[300px] tw-max-h-[300px] tw-w-full tw-transition-transform tw-duration-1000 tw-ease-out group-hover:tw-scale-110"
-                        />
-                    )}
-
-                    <Anchor className="link-overlay" path={path}>
-                        {title}
-                    </Anchor>
-                </figure>
+            <div className="tw-px-[18px]">
                 <div
-                    className="tw-relative tw-px-5 tw-pt-12 tw-pb-4 tw-rounded-2xl"
-                    style={{ direction: "rtl" }}
+                    className={clsx(
+                        "tw-transition-all tw-bg-gray-100 tw-rounded-[15px] tw-h-full group hover:tw-bg-white hover:tw-shadow-4xl hover:tw-shadow-black/[0.12]",
+                        className
+                    )}
+                    ref={ref}
                 >
-                    <h3 className="tw-capitalize tw-font-bold tw-text-violet-dark tw-bg-[#E4E4E4] tw-bg-opacity-90  tw-backdrop-blur-[10px] tw-leading-none tw-rounded-[15px] tw-flex tw-justify-center tw-items-center tw-absolute -tw-right-4 -tw-translate-y-1/2 tw-top-0 tw-w-[calc(100%_+_34px)] tw-h-[60px] md:tw-h-[70px] tw-text-lg md:tw-text-base tw-py-6 tw-px-5">
-                        <Anchor path={path}>{title}</Anchor>
-                    </h3>
-                    {/* <span className="tw-font-medium tw-block tw-uppercase tw-mb-1 tw-tracking-[2px] tw-text-secondary-light">
-                        {dayjs(published_at).format("MMM DD, YYYY")}
-                    </span> */}
-                    {/* {excerpt && <p className="tw-mt-2.5">{excerpt}</p>} */}
-                    <div className="tw-w-full tw-flex tw-items-center tw-justify-between">
-                        <div className="tw-flex tw-items-center tw-flex-nowrap">
-                            <NineOclock className="tw-ml-2" />
-                            <span> {duration} </span>
-                        </div>
-                        <div className="tw-flex tw-items-center">
-                            <ManSpeaking className="tw-ml-2" />
-                            <span> {instructorName} </span>
-                        </div>
-                        <div className="tw-flex tw-items-center">
-                            <BitcoinIcon className="tw-ml-2" />
-                            <span> {price} هزار تومان </span>
+                    <figure className="tw-relative tw-overflow-hidden tw-rounded-t-[15px]">
+                        {thumbnail?.src && (
+                            <img
+                                src={thumbnail.src}
+                                alt={thumbnail?.alt || title}
+                                width={thumbnail?.width || 300}
+                                height={thumbnail?.height || 300}
+                                loading={thumbnail?.loading || "lazy"}
+                                className="tw-min-h-[300px] tw-max-h-[300px] tw-w-full tw-transition-transform tw-duration-1000 tw-ease-out group-hover:tw-scale-110"
+                            />
+                        )}
+
+                        <Anchor className="link-overlay" path={path}>
+                            {title}
+                        </Anchor>
+                    </figure>
+                    <div
+                        className="tw-relative tw-px-5 tw-pt-12 tw-pb-4 tw-rounded-2xl"
+                        style={{ direction: "rtl" }}
+                    >
+                        <h3 className="tw-capitalize tw-font-bold tw-text-violet-dark tw-bg-[#E4E4E4] tw-bg-opacity-90  tw-backdrop-blur-[10px] tw-leading-none tw-rounded-[15px] tw-flex tw-justify-center tw-items-center tw-absolute -tw-right-4 -tw-translate-y-1/2 tw-top-0 tw-w-[calc(100%_+_34px)] tw-h-[60px] md:tw-h-[70px] tw-text-lg md:tw-text-base tw-py-6 tw-px-5">
+                            <Anchor path={path}>{title}</Anchor>
+                        </h3>
+                        {/* <span className="tw-font-medium tw-block tw-uppercase tw-mb-1 tw-tracking-[2px] tw-text-secondary-light">
+                            {dayjs(published_at).format("MMM DD, YYYY")}
+                        </span> */}
+                        {/* {excerpt && <p className="tw-mt-2.5">{excerpt}</p>} */}
+                        <div className="tw-w-full tw-flex tw-items-center tw-justify-between tw-text-sm">
+                            <div className="tw-flex tw-items-center tw-flex-nowrap">
+                                <NineOclock className="tw-ml-2" />
+                                <span> {duration} </span>
+                            </div>
+                            <div className="tw-flex tw-items-center">
+                                <ManSpeaking className="tw-ml-2" />
+                                <span> {instructorName} </span>
+                            </div>
+                            <div className="tw-flex tw-items-center">
+                                <BitcoinIcon className="tw-ml-2" />
+                                <span> {price} هزار تومان </span>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         );
     }
